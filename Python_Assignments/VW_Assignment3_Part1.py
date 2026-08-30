7834c90c61a99c1db934ff1c813b95322e077024
VaylenW <vaylenjwilkerson@gmail.com>
2026-08-30 16:17:47 -0400

    Using f-strings and a renaming of the variables

--- Python_Assignments/VW_Assignment3_Part1.py
+++ Python_Assignments/VW_Assignment3_Part1.py
@@ -1,13 +1,14 @@
 #Program_1
-gc_balance = float(input("Hello! Please input the starting balance of your giftcard: "))
-print("The current balance on your gift card is $" + str(round(gc_balance)) + ".")
-gc_purchase1 = float(input("What is the cost of your first purchase?: $"))
-gc_purchase1 = gc_purchase1 + gc_purchase1*.06
-gc_balance = gc_balance - gc_purchase1
-print("The total amount of money subtracted from your giftcard after tax is: $" + str(round(gc_purchase1,2)) + ".")
-print("The current balance on your gift card is $" + str(round(gc_balance,2)) + ".")
-gc_purchase2 = float(input("What is the cost of your second purchase?: $"))
-gc_purchase2 = gc_purchase2 + gc_purchase2*.06
-gc_balance = gc_balance - gc_purchase2
-print("The total amount of money subtracted from your giftcard after tax is: $" + str(round(gc_purchase2,2)) + ".")
-print("The current balance on your gift card is $" + str(round(gc_balance)) + ".")
+card_balance = float(input("Hello! Please input the starting balance of your giftcard: "))
+#Watched a video that mentioned f-strings so i wanted to try them out here
+print(f"The current balance on your gift card is $ {round(card_balance,2)} .")
+card_purchase1 = float(input("What is the cost of your first purchase?: $"))
+card_purchase1 = card_purchase1 + card_purchase1*.06
+card_balance = card_balance - card_purchase1
+print(f"The total amount of money subtracted from your giftcard after tax is: ${round(card_purchase1,2)} .")
+print(f"The current balance on your gift card is $ {round(card_balance,2)} .")
+card_purchase2 = float(input("What is the cost of your second purchase?: $"))
+card_purchase2 = card_purchase2 + card_purchase2*.06
+card_balance = card_balance - card_purchase2
+print(f"The total amount of money subtracted from your giftcard after tax is: $ {round(card_purchase2,2)} .")
+print(f"The current balance on your gift card is $ {round(card_balance,2)} .")
