70fe5ff4bb6e6aef2d2181e3fb7625ed1ab675bc
VaylenW <vaylenjwilkerson@gmail.com>
2026-08-31 21:21:29 -0400

    assignment 3 bug fix

--- Python_Assignments/VW_Assignment3_Part1.py
+++ Python_Assignments/VW_Assignment3_Part1.py
@@ -1,7 +1,7 @@
 #Program_1
-card_balance = float(input("Hello! Please input the starting balance of your giftcard: "))
+card_balance = float(input("Hello! Please input the starting balance of your giftcard: $"))
 #Watched a video that mentioned f-strings so i wanted to try them out here
-print(f"The current balance on your gift card is$ {format(card_balance, ",.2f")}.")
+print(f"The current balance on your gift card is ${format(card_balance, ",.2f")}.")
 card_purchase1 = float(input("What is the cost of your first purchase?: $"))
 card_purchase1 = card_purchase1 + card_purchase1*.06
 card_balance = card_balance - card_purchase1
